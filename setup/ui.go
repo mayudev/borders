c655e1e6cfc6bf0fe2d0acd252fe4c4065ac9c7b
Mélusine <melusine.hermier@proton.me>
2025-05-24 18:39:09 +0200

    complete setup endpoints functionality

--- setup/ui.go
+++ setup/ui.go
@@ -21,6 +21,24 @@ type pageInfo struct {
 	Countries  []models.CountryBase
 }
 
+type setupDTO struct {
+	Username             string `form:"username" binding:"required"`
+	Password             string `form:"password" binding:"required"`
+	PasswordConfirmation string `form:"password-confirm" binding:"required"`
+}
+
+var didSetup = false
+
+func notTwice() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if didSetup {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "setup already completed successfully - restart the application to make the setup endpoints go away",
+			})
+		}
+	}
+}
+
 func ensureKey(key string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var params keyDTO
@@ -52,9 +70,46 @@ func show(key string) gin.HandlerFunc {
 
 func run(db *gorm.DB) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if err := Setup(db, "bwaa", "bwaa"); err != nil {
-			ctx.AbortWithError(http.StatusInternalServerError, err)
+		var params setupDTO
+
+		if err := ctx.ShouldBind(&params); err != nil {
+			ctx.Error(err)
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "missing parameters in the request",
+			}) // this should respect the users dark / light setting (a webpage with just text might not).
 			return
 		}
+
+		if params.Password != params.PasswordConfirmation {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "password doesn't match the confirmation",
+			}) // this should respect the users dark / light setting (a webpage with just text might not).
+			return
+		}
+
+		if len(params.Password) < 4 {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "please use a password with more than four characters", // not technically correct if a multi-byte character is used.
+			}) // this should respect the users dark / light setting (a webpage with just text might not).
+			return
+		}
+
+		if len(params.Username) < 1 {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "please do not use an empty username",
+			}) // this should respect the users dark / light setting (a webpage with just text might not).
+			return
+		}
+
+		if err := Setup(db, params.Username, params.Password); err != nil {
+			ctx.Error(err)
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": "error setting up the application - please check the server log",
+			})
+			return
+		}
+
+		didSetup = true
+		ctx.Redirect(http.StatusFound, "/")
 	}
 }
