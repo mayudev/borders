3b8b6d144e567e87d6ac88c345717deb145be375
Mélusine <melusine.hermier@proton.me>
2025-05-24 22:02:27 +0200

    create, check JWTs

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,15 +1,84 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/melsincostan/borders/auth/key"
 )
 
 const (
 	validity = 1 * time.Hour
+	issuer   = "borders-app-auth"
 )
 
-func Create() (token jwt.Token) {
+var (
+	ErrUnparseableClaims = errors.New("claims could not be cast into the expected format")
+	ErrExpired           = errors.New("the token expired")
+	ErrTooNew            = errors.New("the token cannot be used yet")
+	ErrWrongIssuer       = errors.New("the issuer doesn't match the expected value")
+	ErrIssuedInTheFuture = errors.New("the token was issued in the future")
+	ErrValidityTooLong   = errors.New("the validity period of the token is too long")
+)
+
+func Create() (signedToken string, err error) {
+	ct := time.Now()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss": issuer,
+		"iat": ct.Unix(),
+		"nbf": ct.Add(-(5 * time.Second)).Unix(),
+		"exp": ct.Add(validity).Unix(),
+	})
+	return token.SignedString(key.Get())
+}
+
+func Check(signedToken string) (err error) {
+	token, err := jwt.Parse(signedToken, key.Func())
+	if err != nil || !token.Valid {
+		return fmt.Errorf("could not parse token: %w", err)
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return ErrUnparseableClaims
+	}
+
+	exp, err := claims.GetExpirationTime()
+	if err != nil {
+		return fmt.Errorf("%w (exp)", err)
+	}
+	if exp.Before(time.Now()) {
+		return ErrExpired
+	}
+
+	nbf, err := claims.GetNotBefore()
+	if err != nil {
+		return fmt.Errorf("%w (nbf)", err)
+	}
+	if nbf.Before(time.Now()) {
+		return ErrTooNew
+	}
+
+	iat, err := claims.GetIssuedAt()
+	if err != nil {
+		return fmt.Errorf("%w (nbf)", err)
+	}
+	if iat.Before(time.Now()) {
+		return ErrIssuedInTheFuture
+	}
+
+	iss, err := claims.GetIssuer()
+	if err != nil {
+		return fmt.Errorf("%w (iss)", err)
+	}
+	if iss != issuer {
+		return ErrWrongIssuer
+	}
+
+	if exp.Sub(iat.Time) > validity {
+		return ErrValidityTooLong
+	}
+
 	return
 }
--- auth/key/key.go
+++ auth/key/key.go
@@ -1,16 +1,32 @@
 package key
 
 import (
+	"errors"
 	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var key = []byte{}
+
+var (
+	ErrWrongAlgorithm = errors.New("token uses the wrong algorithm for the key")
+)
 
 func Get() []byte {
 	if len(key) < 1 {
 		panic(fmt.Errorf("trying to use an uninitialized or empty JWT signing key"))
 	}
 	return key
+}
+
+func Func() jwt.Keyfunc {
+	return func(t *jwt.Token) (interface{}, error) {
+		if t.Header["alg"] != jwt.SigningMethodHS256.Alg() {
+			return nil, ErrWrongAlgorithm
+		}
+		return Get(), nil
+	}
 }
 
 type Config struct {
