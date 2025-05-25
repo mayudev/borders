12b32a2d4d7d36e356e4a89b31abf069ef89717d
Mélusine <melusine.hermier@proton.me>
2025-05-25 21:04:24 +0200

    countries in alphabetical order

--- db/country/default.go
+++ db/country/default.go
@@ -3,14 +3,14 @@ package country
 import "github.com/melsincostan/borders/db/models"
 
 var Default = []models.CountryBase{
-	{Name: "Germany"},
-	{Name: "France"},
-	{Name: "Poland"},
 	{Name: "Austria"},
-	{Name: "Switzerland"},
-	{Name: "Luxembourg"},
-	{Name: "Denmark"},
-	{Name: "Czech Republic"},
-	{Name: "Netherlands"},
 	{Name: "Belgium"},
+	{Name: "Czech Republic"},
+	{Name: "Denmark"},
+	{Name: "France"},
+	{Name: "Germany"},
+	{Name: "Luxembourg"},
+	{Name: "Netherlands"},
+	{Name: "Poland"},
+	{Name: "Switzerland"},
 }
